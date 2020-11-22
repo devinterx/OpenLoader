23c6696a5c975a970af8fe8b9e53e2817f953f71
Dimitriy Kalugin <devinterx@gmail.com>
2020-11-22 21:39:55 +0300

    + Orbit Camera's mixins

---
+++ Assets/CameraManager/OrbitCamera.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+namespace OpenUniverse.CameraManager
+{
+    [RequireComponent(typeof(Camera))]
+    public class OrbitCamera : MonoBehaviour
+    {
+        public Transform target;
+        public float distance = 5.0f;
+        public float xSpeed = 120.0f;
+        public float ySpeed = 120.0f;
+
+        public float yMinLimit = -20f;
+        public float yMaxLimit = 80f;
+
+        public float zoomStep = 1f;
+
+        public float distanceMin = .5f;
+        public float distanceMax = 15f;
+        private float _x;
+        private float _y;
+
+        private void LateUpdate()
+        {
+            if (!target || !Input.GetButton("Fire1") && Math.Abs(Input.mouseScrollDelta.y) < 0.001f) return;
+        
+            var targetPosition = target.position;
+        
+            _x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+            _y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+        
+            _y = ClampAngle(_y, yMinLimit, yMaxLimit);
+        
+            var rotation = Quaternion.Euler(_y, _x, 0);
+        
+            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * zoomStep, distanceMin, distanceMax);
+        
+            if (Physics.Linecast(targetPosition, transform.position, out var hit))
+            {
+                distance -= hit.distance;
+            }
+        
+            var negotiateDistance = new Vector3(0.0f, 0.0f, -distance);
+            var position = rotation * negotiateDistance + targetPosition;
+        
+            transform.rotation = rotation;
+            // var offsetZ = distance * Mathf.Tan(Mathf.Deg2Rad * 90f - 70f);
+            // transform.position = new Vector3(position.x, position.y, position.z - offsetZ);
+            transform.position = position;
+        }
+        
+        private static float ClampAngle(float angle, float min, float max)
+        {
+            if (angle < -360f) angle += 360f;
+            if (angle > 360f) angle -= 360f;
+        
+            return Mathf.Clamp(angle, min, max);
+        }
+    }
+}
---
+++ Assets/CameraManager/SidedOrbitCamera.cs
@@ -0,0 +1,74 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace OpenUniverse.CameraManager
+{
+    public class SidedOrbitCamera : MonoBehaviour
+    {
+        private bool _isDirty = true;
+
+        [SerializeField]
+        private GameObject target;
+
+        [SerializeField, SerializeProperty("HorizontalOffset")]
+        private HorizontalOrbitCameraOffset horizontalOffset = HorizontalOrbitCameraOffset.None;
+
+        [UsedImplicitly]
+        private HorizontalOrbitCameraOffset HorizontalOffset
+        {
+            get => horizontalOffset;
+            set
+            {
+                horizontalOffset = value;
+                _isDirty = true;
+            }
+        }
+
+        private Camera _camera;
+        private Vector3 _cameraOffset = new Vector3(0, 0, 0);
+        private Transform _cameraTransform;
+        private Transform _originTransform;
+
+        public void Awake()
+        {
+            if (_camera == null) _camera = transform.GetComponent<Camera>();
+            if (_cameraTransform == null) _cameraTransform = transform;
+            if (_originTransform == null) _originTransform = new GameObject().transform;
+        }
+
+        public void Update()
+        {
+            if (_camera == null || target == null || !_isDirty) return;
+
+            var originCameraPosition = _cameraTransform.position - _cameraTransform.TransformDirection(_cameraOffset);
+
+            _originTransform.position = originCameraPosition;
+
+            var distance = Vector3.Distance(
+                _originTransform.InverseTransformPoint(originCameraPosition),
+                _originTransform.InverseTransformPoint(target.transform.position)
+            );
+
+            var frustumHeight = 2.0f * distance * Mathf.Tan(_camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+            var frustumWidth = frustumHeight * _camera.aspect;
+
+            _cameraOffset.x = horizontalOffset == HorizontalOrbitCameraOffset.FullLeftSide ? frustumWidth / 2
+                : horizontalOffset == HorizontalOrbitCameraOffset.HalfLeftSide ? frustumWidth / 4
+                : horizontalOffset == HorizontalOrbitCameraOffset.FullRightSide ? -frustumWidth / 2
+                : horizontalOffset == HorizontalOrbitCameraOffset.HalfRightSide ? -frustumWidth / 4 : 0;
+
+            _cameraTransform.position = originCameraPosition + _cameraTransform.TransformDirection(_cameraOffset);
+
+            _isDirty = false;
+        }
+    }
+
+    internal enum HorizontalOrbitCameraOffset
+    {
+        FullLeftSide,
+        HalfLeftSide,
+        None,
+        HalfRightSide,
+        FullRightSide
+    }
+}
